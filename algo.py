8c152be2ebed80b0fb8ceac5281096700aec7904
Shubham210204 <shubhamkalra929@gmail.com>
2025-08-10 23:35:47 +0530

    edited env

--- algo.py
+++ algo.py
@@ -8,7 +8,7 @@ ist = pytz.timezone('Asia/Kolkata')
 
 while True:
     now = datetime.datetime.now(ist)  # get current date and time in IST
-    print(f"Current date and time: {now.strftime('%Y-%m-%d %H:%M:%S')}")
+    print(f"Alive since: {now.strftime('%Y-%m-%d %H:%M:%S')}")
     sys.stdout.flush()
     time.sleep(3600)  # wait 1 hour
 
