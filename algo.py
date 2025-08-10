7a04872cc9625cb9dc96410f831161cdc0d49ae1
Shubham210204 <shubhamkalra929@gmail.com>
2025-08-10 23:12:36 +0530

    edited algo

--- algo.py
+++ algo.py
@@ -1,8 +1,18 @@
+# import time
+# import random
+# import sys
+
+# while True:
+#     print(f"Running algo... Value: {random.randint(1,100)}")
+#     sys.stdout.flush()  # flush immediately
+#     time.sleep(2)
+
 import time
-import random
+import datetime
 import sys
 
 while True:
-    print(f"Running algo... Value: {random.randint(1,100)}")
+    now = datetime.datetime.now()  # get current date and time
+    print(f"Alive from: {now.strftime('%Y-%m-%d %H:%M:%S')}")
     sys.stdout.flush()  # flush immediately
-    time.sleep(2)
+    time.sleep(3600)  # wait 1 hour (3600 seconds)
